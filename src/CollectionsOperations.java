1ec7e17afb2071e951bdc186bbf22d89b84f9b5d
RedGalm <65945889+RedGalm@users.noreply.github.com>
2025-04-13 14:06:31 +0200

    Changes

--- src/CollectionsOperations.java
+++ src/CollectionsOperations.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 public interface CollectionsOperations {
+
+    public void readUserData(Map<String, ArrayList<UserData>> users);
 
 }
