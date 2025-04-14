a5b50890df27b073c3ce2db4213725584fd3ec02
RedGalm <65945889+RedGalm@users.noreply.github.com>
2025-04-15 00:32:27 +0200

    Changes

--- src/TextUI.java
+++ src/TextUI.java
@@ -1,6 +1,37 @@
-import java.util.Scanner;
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class TextUI {
     private Scanner reader;
+    private Map<String, ArrayList<UserData>> users;
+    private List<MusicAlbum> musicAlbumList;
+    private List<Artist> artistList;
+
+    public TextUI(Scanner reader) {
+        this.reader = reader;
+        this.users = new HashMap<>();
+        this.musicAlbumList = new ArrayList<>();
+        this.artistList = new ArrayList<>();
+
+        System.out.println("Enter data filename");
+        File fileName = new File(reader.nextLine());
+        String path = fileName.getAbsolutePath();
+
+        try (Scanner scanner = new Scanner(Paths.get(path))) {
+            while (scanner.hasNextLine()) {
+
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+
+
+    public int HashCode(String username) {
+        return username.hashCode();
+    }
+
 
 }
