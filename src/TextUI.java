09aff13c1ee58307ec82323112a4f07845e5b314
RedGalm <65945889+RedGalm@users.noreply.github.com>
2025-04-16 15:34:17 +0200

    Changes

--- src/TextUI.java
+++ src/TextUI.java
@@ -20,10 +20,12 @@ public class TextUI {
 
         try (Scanner scanner = new Scanner(Paths.get(path))) {
             int lineNum = 0;
+
             while (scanner.hasNextLine()) {
                 String row = scanner.nextLine();
                 lineNum++;
                 String[] arr = row.split(",");
+
                 if (arr[0].equalsIgnoreCase("userAndAlbumData")) {
                     hashCode(arr[1]);
                     users.putIfAbsent(arr[1], new ArrayList<>());
@@ -32,13 +34,15 @@ public class TextUI {
                         musicAlbumList.add(album);
                     }
                     users.get(arr[1]).add(new User(album, Integer.parseInt(arr[4]), Double.parseDouble(arr[5])));
+
                 } else if (arr[0].equalsIgnoreCase("artistData")) {
                     Artist artist = new Artist(arr[1], Integer.parseInt(arr[2]));
                     if (!(this.artistList.contains(artist))) {
                         this.artistList.add(artist);
                     }
+
                 } else {
-                    System.out.println("Error: Unspecified data at line " + lineNum);
+                    System.out.println("Error: Unspecified data at the line " + lineNum);
                 }
             }
         } catch (Exception e) {
@@ -48,18 +52,69 @@ public class TextUI {
     }
 
     public void start() {
-        while (true) {
-            System.out.println("Commands list:" +
-                    "\nexit - closes program" +
-                    "\nsort - sorts data by order" +
-                    "\n");
+        String command = "";
+        while (!(command.equalsIgnoreCase("exit"))) {
+            System.out.println("Choose data set:" +
+                    "\n1 - Users" +
+                    "\n2 - MusicAlbums" +
+                    "\n3 - Artists" +
+                    "\nexit - closes program");
+            command = reader.nextLine();
+            if (command.equalsIgnoreCase("Users")) {
+                System.out.println("Commands list:" +
+                        "\nuserInfo - search for specified user" +
+                        "\nprintAll - prints all data" +
+                        "\nexit - closes program");
+                command = reader.nextLine();
+                if (command.equalsIgnoreCase("userInfo")) {
+                    System.out.println("Enter user name");
+                    String user = reader.nextLine();
+                    this.users.get(user).forEach(System.out::println);
+                }
+
+                if (command.equalsIgnoreCase("printAll")) {
+                    for (String key: this.users.keySet()) {
+                        this.users.get(key).forEach(System.out::println);
+                    }
+                }
+            }
+
+            if (command.equalsIgnoreCase("MusicAlbums") || command.equalsIgnoreCase("Artists")) {
+                List<?> selectedList = new ArrayList<>();
+                if (command.equalsIgnoreCase("MusicAlbums")) {
+                    selectedList = this.musicAlbumList;
+                } else {
+                    selectedList = this.artistList;
+                }
+
+                System.out.println("Commands list:" +
+                        "\nexit - closes program" +
+                        "\nsort - sorts data by order" +
+                        "\nsearch - search by specified word" +
+                        "\nprintAll - prints all data");
+
+                command = reader.nextLine();
+
+                switch (command) {
+                    case "exit":
+                        break;
+                    case "sort":
+
+                }
+
+            }
+
+
+
         }
     }
-
 
     public int hashCode(String username) {
         return username.hashCode();
     }
+
+
+
 
 
 }
